18740c0979d8797b8847fe8ce7f789f67377f7dd
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2023-02-05 12:03:40 +0000

    Music to main menu

--- Assets/Scripts/UI/Buttons.cs
+++ Assets/Scripts/UI/Buttons.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Buttons : MonoBehaviour
-{
-  private string sceneToLoad;
-   public void QuitGame()
-  {
-    Application.Quit();
-    Debug.Log("Quit Game");
-  }
+public class Buttons : MonoBehaviour {
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private GameObject quitButton;
+    private void Awake() {
+#if UNITY_WEBGL
+        Destroy(quitButton);
+#endif
+    }
 
-  public void PlayGame()
-  {
-    sceneToLoad = "Gameplay";
-    SceneManager.LoadScene(sceneToLoad);
-  }
+    public void QuitGame() {
+        audioSource.Play();
+        Application.Quit();
+    }
+
+    public void PlayGame() {
+        audioSource.Play();
+        SceneManager.LoadScene(1);
+    }
 }
